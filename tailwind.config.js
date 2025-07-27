26608952b1bfc2d8d7bac54d5e96eecce22bc48c
samar <semomoamer38@gmail.com>
2025-07-27 21:04:24 +0300

    Initial commit - Tailwind project

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  content: ["./*.html"],
+  darkMode: 'class', // للتحكم بالوضع الداكن عبر class
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['Segoe UI', 'Tahoma', 'Geneva', 'Verdana', 'sans-serif'],
+      },
+    },
+  },
+  plugins: [],
+}
